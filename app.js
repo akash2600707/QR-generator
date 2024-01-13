83b592df414ac78b580c37ec15b07db809780fdf
Akash ramesh <59521490+akash2600707@users.noreply.github.com>
2024-01-13 19:39:16 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const qrcode = require('qrcode');
-const jsQR = require('jsqr');
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -23,17 +22,6 @@ app.get('/generateQR', (req, res) => {
         }
         res.send(url);
     });
-});
-
-app.post('/scanQR', (req, res) => {
-    const { data, width, height } = req.body;
-    const code = jsQR(data, width, height);
-
-    if (code) {
-        res.json({ result: code.data });
-    } else {
-        res.status(400).json({ error: 'QR code not found or invalid.' });
-    }
 });
 
 app.listen(port, () => {
